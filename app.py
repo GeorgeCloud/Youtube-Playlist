f47fa256f2ef441ea5dab2ac4fdb8192e812aec7
George Ceja <Net.george.sky@gmail.com>
2021-10-22 22:43:58 -0700

    [add] base jinja code for base template

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'main page'
+    return render_template('index.html', playlist_name='our')
+
+
+# User model
+# Playlist model
 
 
 if __name__ == '__main__':
