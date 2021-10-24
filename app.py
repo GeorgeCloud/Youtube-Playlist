34305df4827568c5f92235c5c7cf30e79b4b3758
George Ceja <Net.george.sky@gmail.com>
2021-10-24 11:54:52 -0700

    add MongoDB

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
+client = MongoClient()
+db = client.Playlister
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', playlist_name='our')
+    # TODO: return -head 10 > find()
+    return render_template('view_playlists.html', playlists=playlists.find())
 
-
-# User model
-# Playlist model
+# @app.route('playlist/<int:id>', methods=['GET'])
+# def view_playlist(id):
+#     # playlist = DB query here
+#     return render_template('view_playlist.html', playlist=playlist)
 
 
 if __name__ == '__main__':
